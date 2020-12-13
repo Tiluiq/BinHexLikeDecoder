2af89bd324f610db342c0b816536570ef0347b65
Tiluiq <63489860+Tiluiq@users.noreply.github.com>
2020-12-13 12:27:03 +0900

    公開用

--- BinHexLikeDecoder/BinHexLikeDecoder/Form1.Designer.cs
+++ BinHexLikeDecoder/BinHexLikeDecoder/Form1.Designer.cs
@@ -29,13 +29,111 @@ namespace BinHexLikeDecoder
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BinHexLabel = new System.Windows.Forms.Label();
+            this.ByteLabel = new System.Windows.Forms.Label();
+            this.CopyButton = new System.Windows.Forms.Button();
+            this.ASCIILabel = new System.Windows.Forms.Label();
+            this.BinHexTextBox = new System.Windows.Forms.TextBox();
+            this.ByteRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.ASCIIRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // BinHexLabel
+            // 
+            this.BinHexLabel.AutoSize = true;
+            this.BinHexLabel.Location = new System.Drawing.Point(13, 13);
+            this.BinHexLabel.Name = "BinHexLabel";
+            this.BinHexLabel.Size = new System.Drawing.Size(106, 12);
+            this.BinHexLabel.TabIndex = 0;
+            this.BinHexLabel.Text = "BinHexっぽいテキスト";
+            // 
+            // ByteLabel
+            // 
+            this.ByteLabel.AutoSize = true;
+            this.ByteLabel.Location = new System.Drawing.Point(470, 13);
+            this.ByteLabel.Name = "ByteLabel";
+            this.ByteLabel.Size = new System.Drawing.Size(44, 12);
+            this.ByteLabel.TabIndex = 2;
+            this.ByteLabel.Text = "バイト列";
+            // 
+            // CopyButton
+            // 
+            this.CopyButton.Location = new System.Drawing.Point(713, 416);
+            this.CopyButton.Name = "CopyButton";
+            this.CopyButton.Size = new System.Drawing.Size(75, 23);
+            this.CopyButton.TabIndex = 4;
+            this.CopyButton.Text = "コピー";
+            this.CopyButton.UseVisualStyleBackColor = true;
+            this.CopyButton.Click += new System.EventHandler(this.CopyButton_Click);
+            // 
+            // ASCIILabel
+            // 
+            this.ASCIILabel.AutoSize = true;
+            this.ASCIILabel.Location = new System.Drawing.Point(792, 13);
+            this.ASCIILabel.Name = "ASCIILabel";
+            this.ASCIILabel.Size = new System.Drawing.Size(34, 12);
+            this.ASCIILabel.TabIndex = 5;
+            this.ASCIILabel.Text = "ASCII";
+            // 
+            // BinHexTextBox
+            // 
+            this.BinHexTextBox.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.BinHexTextBox.Location = new System.Drawing.Point(12, 29);
+            this.BinHexTextBox.Multiline = true;
+            this.BinHexTextBox.Name = "BinHexTextBox";
+            this.BinHexTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.BinHexTextBox.Size = new System.Drawing.Size(454, 381);
+            this.BinHexTextBox.TabIndex = 7;
+            this.BinHexTextBox.TextChanged += new System.EventHandler(this.BinHexTextBox_TextChanged);
+            // 
+            // ByteRichTextBox
+            // 
+            this.ByteRichTextBox.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.ByteRichTextBox.Location = new System.Drawing.Point(472, 28);
+            this.ByteRichTextBox.Name = "ByteRichTextBox";
+            this.ByteRichTextBox.ReadOnly = true;
+            this.ByteRichTextBox.Size = new System.Drawing.Size(316, 381);
+            this.ByteRichTextBox.TabIndex = 10;
+            this.ByteRichTextBox.Text = "";
+            // 
+            // ASCIIRichTextBox
+            // 
+            this.ASCIIRichTextBox.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.ASCIIRichTextBox.Location = new System.Drawing.Point(794, 29);
+            this.ASCIIRichTextBox.Name = "ASCIIRichTextBox";
+            this.ASCIIRichTextBox.ReadOnly = true;
+            this.ASCIIRichTextBox.Size = new System.Drawing.Size(129, 381);
+            this.ASCIIRichTextBox.TabIndex = 11;
+            this.ASCIIRichTextBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(935, 450);
+            this.Controls.Add(this.ASCIIRichTextBox);
+            this.Controls.Add(this.ByteRichTextBox);
+            this.Controls.Add(this.BinHexTextBox);
+            this.Controls.Add(this.ASCIILabel);
+            this.Controls.Add(this.CopyButton);
+            this.Controls.Add(this.ByteLabel);
+            this.Controls.Add(this.BinHexLabel);
+            this.Name = "Form1";
+            this.Text = "BinHexLikeDecoder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label BinHexLabel;
+        private System.Windows.Forms.Label ByteLabel;
+        private System.Windows.Forms.Button CopyButton;
+        private System.Windows.Forms.Label ASCIILabel;
+        private System.Windows.Forms.TextBox BinHexTextBox;
+        private System.Windows.Forms.RichTextBox ByteRichTextBox;
+        private System.Windows.Forms.RichTextBox ASCIIRichTextBox;
     }
 }
 
--- BinHexLikeDecoder/BinHexLikeDecoder/Form1.cs
+++ BinHexLikeDecoder/BinHexLikeDecoder/Form1.cs
@@ -16,5 +16,130 @@ namespace BinHexLikeDecoder
         {
             InitializeComponent();
         }
+
+        //binhexで使われる文字種　位置が変換後の数値(0～63)に対応
+        static string chars = "!\"#$%&'()*+,-012345689@ABCDEFGHIJKLMNPQRSTUVXYZ[`abcdefhijklmpqr";
+
+        //binhexっぽいテキストをバイト配列に変換
+        private byte[] BinHexLike2Bytes(string binhexText)
+        {
+            binhexText = binhexText.Replace("\r\n", "\n");
+            string[] lines = binhexText.Split('\n');
+            if (lines[0] == "(This file must be converted with BinHex 4.0)")
+            {
+                //先頭行を削除
+                List<string> tmpLines = new List<string>(lines);
+                tmpLines.RemoveAt(0);
+                lines = tmpLines.ToArray();
+            }
+            //文字部分すべてつなげる
+            string binhexStr = string.Join("", lines);
+            //前後の:除去
+            binhexStr = binhexStr.Trim(':');
+
+            List<byte> byteList = new List<byte>();
+
+            for (int i = 0; i < binhexStr.Length / 4; i++)
+            {
+                byte[] bytes = new byte[3];
+                int[] indices = new int[4];
+                //文字→数値変換
+                for (int j = 0; j < 4; j++)
+                {
+                    indices[j] = chars.IndexOf(binhexStr[i * 4 + j]);
+                    if (indices[j] < 0)
+                        return new byte[1];    //不正な文字が来たら長さ1の配列返す
+                }
+
+                //何故かシフト演算使ってなかった時の
+                bytes[0] = (byte)(indices[0] * 4 + indices[1] / 16);
+                bytes[1] = (byte)(indices[1] % 16 * 16 + indices[2] / 4);
+                bytes[2] = (byte)(indices[2] % 4 * 64 + indices[3]);
+
+                bytes[0] = (byte)((indices[0] << 2) + (indices[1] >> 4));
+                bytes[1] = (byte)(((indices[1] & 0x0F) << 4) + (indices[2] >> 2));
+                bytes[2] = (byte)(((indices[2] & 0x03) << 6) + indices[3]);
+
+                byteList.AddRange(bytes);
+            }
+
+            //ランレングス展開 
+            for (int i = 0; i < byteList.Count; i++)
+            {
+                if (byteList[i] == 0x90)
+                {
+                    if (byteList[i + 1] == 0x00)
+                    {
+                        //0x90 0x00なら0x00消す
+                        byteList.RemoveAt(i + 1);
+                        continue;
+                    }
+                    //0x90削除
+                    byteList.RemoveAt(i);
+                    //繰り返し数取得
+                    int repeat = byteList[i];
+                    //繰り返し数削除
+                    byteList.RemoveAt(i);
+                    i--;
+                    //iに繰り返すものが入っているので繰り返し追加(元から1つ入っているので-1回)
+                    for (int j = 0; j < repeat - 1; j++)
+                    {
+                        byteList.Insert(i, byteList[i]);
+                        i++;
+                    }
+                }
+            }
+
+            return byteList.ToArray();
+        }
+
+        private void BinHexTextBox_TextChanged(object sender, EventArgs e)
+        {
+            byte[] decodedBytes = BinHexLike2Bytes(BinHexTextBox.Text);
+
+            string decodedStr = "";
+            for (int i = 0; i < decodedBytes.Length; i++)
+            {
+                decodedStr += BitConverter.ToString(new byte[] { decodedBytes[i] });
+                if ((i + 1) % 16 == 0)
+                    decodedStr += "\r\n";
+                else
+                    decodedStr += " ";
+            }
+
+            ByteRichTextBox.Text = decodedStr;
+
+            //配列コピー
+            byte[] toASCII = new byte[decodedBytes.Length];
+            Array.Copy(decodedBytes, toASCII, decodedBytes.Length);
+
+            for (int i = 0; i < toASCII.Length; i++)
+            {
+                //制御文字と0x7F以降なら.に
+                if (toASCII[i] < 0x20 || 0x7F <= toASCII[i])
+                    toASCII[i] = 0x2E;
+            }
+            string ASCIIstr = "";
+            ASCIIEncoding ascii = new ASCIIEncoding();
+            for (int i = 0; i < toASCII.Length; i++)
+            {
+                ASCIIstr += ascii.GetString(new byte[] { toASCII[i] });
+                if ((i + 1) % 16 == 0)
+                    ASCIIstr += "\r\n";
+            }
+
+            ASCIIRichTextBox.Text = ASCIIstr;
+        }
+
+        private void CopyButton_Click(object sender, EventArgs e)
+        {
+            if (ByteRichTextBox.Text == null || ByteRichTextBox.Text == "") return; //空だったら何もしない
+
+            string tmp = ByteRichTextBox.Text.Replace("\r\n", " ");
+            tmp = tmp.Replace("\r", " ");
+            tmp = tmp.Replace("\n", " ");
+            Clipboard.SetText(tmp);
+        }
+
     }
 }
